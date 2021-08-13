c9677f05355f39a4533df45a1a2872a54481e835
Joshua Hernandez <jedchernandez@gmail.com>
2021-08-12 23:41:59 -0400

    fix: Address comments

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -6,20 +6,22 @@ const asyncHandler = require('express-async-handler');
 exports.createNewProfile = asyncHandler(async (req, res) => {
   const profile = req.body;
   const newProfile = new Profile({ ...profile });
-
-  await newProfile.save();
-  res.status(201).json({ status: 201, profile: newProfile, message: 'Profile created!' });
+  try {
+    await newProfile.save();
+    res.status(201).json({ status: 201, profile: newProfile, message: 'Profile created!' });
+  } catch (error) {
+    res.status(500);
+    throw new Error('Error creating the profile.');
+  }
 });
 
 // UPDATE /profile:id - Update profile with the given id
 exports.updateProfile = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const profile = req.body;
-
+  const options = { new: true };
   try {
-    const updatedProfile = await Profile.findByIdAndUpdate(id, profile, {
-      new: true,
-    });
+    const updatedProfile = await Profile.findByIdAndUpdate(id, profile, options);
     res.status(200).json({ status: 200, profile: updatedProfile, message: 'Profile updated!' });
   } catch (error) {
     res.status(500);
@@ -29,20 +31,30 @@ exports.updateProfile = asyncHandler(async (req, res) => {
 
 // GET /profile:id - Search profile with the given id
 exports.searchProfile = asyncHandler(async (req, res) => {
-  const profile = await Profile.findById(req.params.id);
-  if (!profile) {
-    res.status(404);
+  try {
+    const profile = await Profile.findById(req.params.id);
+    if (profile) {
+      res.status(200).json({ status: 200, profile: profile, message: 'Profile found!' });
+    } else {
+      res.status(404).json({ message: 'Profile not found!' });
+    }
+  } catch (error) {
+    res.status(500);
     throw new Error('Error searching for a profile with that id.');
   }
-  res.status(200).json({ status: 200, profile: profile, message: 'Profile found!' });
 });
 
 // GET /profile - Fetch all existing profiles
 exports.fetchAllProfiles = asyncHandler(async (req, res) => {
-  const profiles = await Profile.find();
-  if (!profiles) {
-    res.status(404);
+  try {
+    const profiles = await Profile.find();
+    if (profiles) {
+      res.status(200).json({ status: 200, profile: profiles, message: 'Profiles found!' });
+    } else {
+      res.status(404).json({ message: 'Profiles not found!' });
+    }
+  } catch (error) {
+    res.status(500);
     throw new Error('Error fetching profiles.');
   }
-  res.status(200).json({ status: 200, profile: profiles, message: 'Profiles found!' });
 });
